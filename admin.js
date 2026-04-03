9c18725602ad4bce4dc556240d9b03af78b4712c
Azz Internet Hub <abdulazizzaroo@gmail.com>
2026-04-03 08:44:33 +0000

    Update admin.js

--- admin.js
+++ admin.js
@@ -5,7 +5,7 @@
 
 // ========== AUTHENTICATION ==========
 const ADMIN_PASSWORD_KEY = 'admin_password_hash';
-const DEFAULT_PASSWORD = 'LordLamba123456';
+const DEFAULT_PASSWORD = 'admin123';
 
 function hashPassword(pwd) {
     let hash = 0;
@@ -72,7 +72,7 @@ function saveProducts() {
 
 // ========== IMPROVED LOAD ORDERS FROM GOOGLE SHEETS ==========
 async function loadOrders() {
-    const webhookUrl = localStorage.getItem('google_sheets_webhook') || '';
+    const webhookUrl = localStorage.getItem('google_sheets_webhook') || 'https://script.google.com/macros/s/AKfycbykGZwSwOZ-OEfEsX56LpD4LBKwjFbSoEkdsFyM-5WnwIIvhEFq_om-O80ZccjwYtnk/exec';
     
     // If no webhook URL, use demo data or localStorage
     if (!webhookUrl) {
